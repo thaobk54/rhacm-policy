747015db92b6f132c21224fe17f76464849e8227
root <thao.vu@stakater.com>
2025-10-29 03:07:35 +0000

    change file

--- scripts/label-managedclusters.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Lấy danh sách các ManagedCluster có tên chứa "childshift"
-CLUSTERS=$(oc get managedclusters -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
-
-for CLUSTER in $CLUSTERS; do
-  echo "Labeling $CLUSTER with environment=childshift"
-  oc label managedcluster $CLUSTER environment=childshift --overwrite
-done
